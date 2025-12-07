af9c0e57dbcc0895ec89e36958ac454f91c074ec
lnaidu14 <lalit.pratap.naidu@gmail.com>
2025-12-07 15:51:48 -0500

    removed unused files

--- src/components/index.jsx
+++ src/components/index.jsx
@@ -1,8 +1,5 @@
-export { default as HomeScreen } from './HomeScreen/HomeScreen';
-export { default as ErrorBoundary } from './ErrorBoundary/ErrorBoundary';
-export { default as TabNavigator } from './TabNavigator/TabNavigator';
+export { default as ErrorBoundary } from "./ErrorBoundary/ErrorBoundary";
 export { default as CampaignsList } from "./CampaignsList/CampaignsList";
-export { default as Tab } from './Tab/Tab';
 export { default as Token } from "./Token/Token";
 export { default as Board } from "./Board/Board";
 export { default as BoardControls } from "./BoardControls/BoardControls";
